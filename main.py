662aab4e109c2ad5bcbb2023705adf2cce03157c
SouptikSinha <souptiksinha1@gmail.com>
2026-05-26 09:51:22 -0500

    fixed middlecors

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["https://travel-companion-frontend-sandy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
